0112705b922311c8f9c189c16e37b81de11ccc45
Spencer Mandzik <spencer.mandzik@gmail.com>
2017-02-24 12:11:06 -0800

    My initial commit for the Caesar Cipher html website

---
+++ README.md
@@ -0,0 +1,22 @@
+## Synopsis
+
+This is an educational project to build a website with Javascript code that can interpret a given input and reproduce an encrypted response. The Caesar cipher, also known as a shift cipher, is one of the simplest forms of encryption. It is a substitution cipher where each letter in the original message (called the plaintext) is replaced with a letter corresponding to a certain number of letters up or down in the alphabet.
+
+
+## Motivation
+
+During the course of my programming journey, I've come across many different brain teaser questions and I wanted to reproduce this with a visual representation. 
+
+## Contributors
+
+This is a project intended to learn Javascript web development. If you would like to add suggestions or advice feel free to reach out. Twitter: @spenncee, or www.mandziks.com
+
+## License
+
+Copyright 2017 Spencer Mandzik
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ main.js
@@ -0,0 +1,82 @@
+console.log("main js is running");
+
+
+function encrypt() {
+  var encryptText = document.getElementById("encrypt1").value;
+  document.getElementById("answer1").innerHTML = encryptText;
+  var ekey = parseInt(document.getElementById("ekey").value);
+  console.log(ekey);
+  console.log(encryptText);
+  console.log(typeof ekey);
+  init(ekey,encryptText);
+  document.getElementById("encrypt1").value = "";
+  document.getElementById("ekey").value = "";
+}
+
+function decrypt() {
+  var decryptText = document.getElementById("encrypt2").value;
+  document.getElementById("ans2").innerHTML = decryptText;
+  var dkey = parseInt(document.getElementById("dkey").value);
+  console.log(dkey);
+  init(dkey,decryptText);
+  document.getElementById("encrypt2").value = "";
+  document.getElementById("dkey").value = "";
+}
+
+caesarCipher = function(offset,string) { 
+     var words    = string.split(" "),
+			word_idx = 0,
+		  
+      arrayWords = [];
+
+	while(word_idx < words.length) {
+		var   word = words[word_idx],
+		letter_idx = 0;
+    new_word = "";
+    while(letter_idx < word.length) {
+    	var char_i = word.charCodeAt(letter_idx) - "a".charCodeAt();
+      if (document.getElementById("encrypt1").value){
+        new_char_i = ((char_i) + offset) % 26;
+      } else {
+        new_char_i = ((char_i) - offset) % 26;
+      }
+    	new_word += String.fromCharCode("a".charCodeAt() + new_char_i);
+    	letter_idx++;
+    }
+    arrayWords.push(new_word);
+    word_idx++;
+	}
+
+
+  if(document.getElementById("encrypt1").value){
+    document.getElementById("answer1").innerHTML = arrayWords.join(" ");
+  } else {
+    document.getElementById("ans1").innerHTML = arrayWords.join(" ");
+  }
+	console.log(arrayWords.join(" "));
+	return arrayWords.join(" ");
+
+};
+
+function init(key,text) {
+	console.log("function initialized...");
+  caesarCipher(key,text);    
+}
+
+
+
+
+
+// function encrypt() {
+//   var encryptText = document.getElementById("encrypt1").value;
+//   document.getElementById("answer1").innerHTML = encryptText;
+//   var ekey = document.getElementById("ekey").value;
+//   console.log(ekey);
+// }
+
+// function decrypt() {
+//   var decryptText = document.getElementById("encrypt2").value;
+//   document.getElementById("answer2").innerHTML = decryptText;
+//   var dkey = document.getElementById("dkey").value;
+//   console.log(dkey);
+// }
